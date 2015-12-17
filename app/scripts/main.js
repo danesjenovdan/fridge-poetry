38ff96772f51f1fb962fc816d0d49d8841d57620
Filip Muki Dobranic <filipdobranic@gmail.com>
2015-12-17 20:08:35 +0100

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,41 @@
+var words = ['levice', 'enake', 'Država', 'jezik', 'stanje', 'migrantskim', 'Prihodnost', 'političnega', 'ali', 'to', 'posebej', 'ti', 'Poroka', 'zoper', 'otroke', 'vodi', 'Spoštovani', 'zagovarja', 'času', 'čas', 'verujoče', 'migrantov', 'vero', 'priseljencev', 'rojstvo', 'Vse', 'napad', 'volilni', 'Vsi', 'tej', 'ga', 'za', 'komunistična', 'odločitve', 'Gre', 'Brisanje', 'ter', 'islama', 'celo', 'večjo', 'zaveda', 'ujela', '1848', 'mogoče', 'k', 'enaki', 'boj', 'temelja', 'katerem', 'otrok', 'radikalnega', 'njihovi', 'družbeni', 'spola', 'narodnost', 'naša', 'pred', 'Levičarji', 'položaj', 'kratkotrajenV', 'integraciji', 'skrajni', 'posega', 'slučajen', 'prebivalcev', 'glas', 'prakticira', 'sploh', 'V', 'govorijo', 'določa', 'enakopravnosti', 'večine', 'v', 'decembra', 'življenja', 'Video', 'bodočih', 'slovenski', 'glasujmo', 'islamskih', 'politiko', 'zvezi', 'pogled', 'napada', 'naroda', 'teroristične', 'pravicIn', 'odvisno', 'odvisna', 'odvisne', 'spol', 'opravičuje', 'zato', 'povečuje', 'gmotno', 'soseščino', 'si', 'so', 'se', 'radikalno', 'radikalni', 'zakonom', 'islam', 'velikim', '20', 'zvezah', 'človekovih', 'agresivne', 'najdražjih', 'domačo', 'pojem', 'dan', 'predsednika', 'ureditev', 'čim', 'podvojilo', 'spodnji', 'veselje', 'hkrati', 'zločinskemu', 'namreč', 'en', 'manifestu', 'Zakona', 'človek', 'citiram', 'povezavi', 'obstaja', 'priseljevanjemV', 'vsebini', 'istospolnimi', 'niti', 'PROTI', 'razvojno', 'obstoj', 'izbire', 'paradoksalno', 'svobodni', 'Evropski', 'drugače', 'okoliščino', 'varstvo', 'življenja', 'šele', 'družina', 'družino', 'stvar', 'zmanjšalo', 'zakona', 'Mesečeva', 'svoboščine', 'že', 'zakonu', 'Prvi', 'uvajanje', 'Ponekod', 'zapisal', 'čeprav', 'katere', 'zadnjih', 'razpoloženje', 'izobrazbo', 'naših', 'sta', 'mladino', 'kot', 'Sloveniji', 'Slovenija', 'izničiti', 'pravicam', 'svojih', 'lahko', 'očetovstvo', 'Erjavčeva', 'evropske', 'nalaga', 'zahodni', 'silijo', 'rojevanjem', 'velja', 'drugih', 'Število', 'podlaga', 'tako', 'točki', 'katerokoli', 'prihodnost', 'proste', 'kak', 'kam', 'demokraciji', 'varuje', 'nastane', 'krepi', 'Otroci', 'ženskami', 'Prihodnosti', 'načelo', 'razmerij', 'ki', 'ko', 'brez', 'demokratski', 'potvarjajo', 'zanika', 'skuša', 'Nasprotno', 'uničujejo', 'vsakomur', 'pomembne', 'pritiskom', 'obnavlja', 'ogledati', 'državi', 'raso', 'prihajaV', 'tudi', 'porazno', 'nasprotujemo', 'vstopajo', 'stojimo', 'istospolnih', 'do', 'odgovornosti', 'trenutno', 'da', 'temeljno', 'enakopravnega', 'omogočile', 'Spoštovani14', 'Cerarjeva', 'temeljne', 'uniji', 'slučajnoKarl', 'misleče', 'razmerjih', 'civilnopravnih', 'števila', 'število', 'stranka', 'prvih', 'pravi»V', 'stranki', 'izseljevanju', 'sodbi', 'tega', 'ustvarja', 'zagotovo', 'jo', 'je', 'izvedbi', 'pojavi', 'zagotovljene', 'še', 's', 'celice', 'celico', 'povečalo', 'materinstvo', 'graditi', 'na', 'temeljnega', 'pod', 'posamezni', 'vedno', 'državah', 'interesa', 'pa', 'družbe', 'po', 'prazninoLevičarjev', 'spoštuje', 'potem', 'drugo', 'družinskih', 'posebno', 'skrivajo', 'oziroma', 'celotna', 'celotni', 'iz', 'moti', 'Pomemben', 'slovensko', 'in', 'slovenske', 'način', 'skupnosti', 'glasov', 'človekove', 'človekova', 'Komunističnem', 'pravice', 'pravica', 'predstavlja', 'volivcev', 'podpiramo', 'SDS', 'Že', 'Židanova', 'pomena', 'ustavi', 'ustava', 'ustave', 'Vendar', 'družbe', 'družba', 'družbo', 'teorije', 'družbi', 'leta', 'ukinile', 'katerim', 'Evropa', 'Evrope', 'napotilu', 'razgrajujejo', 'raste', 'akcije', 'jasno', 'jasna', 'zakonski', 'glede', 'Zagovarjamo', 'Vsakomur', 'odprti', 'njo', 'evropskih', 'politično', 'Marx', 'razmere', 'naš', 'vrednote', 'bistveno', 'diktature', 'narod', 'bo', 'burk', 'uničevanju', 'osebno', 'monopolov', 'uničuje', 'držav', 'od', 'demografski', 'o', 'člen', 'uniči', 'prepričanje', 'naši', 'Na', 'kršitev', 'stehtal', 'dogaja', 'rezultata', 'gospodarsko', 'uživajo', 'sodišča', 'povabilo', 'Z', 'referenduma', 'vzpodbuja', 'temu', 'Evropskega', 'pragu', 'bežen', 'posvojitev', 'Slovenski', 'več', 'z', 'Slovenska', 'pove', 'svoji', 'nista', 'To', 'frazami', 'ni', 'skrb', 'pravic', 'nad', 'manjšine', 'nasilje', 'ne', 'nam', 'različnimi', 'zemljevid', 'sledijo', 'zvesto', 'udeležimo', 'predlog', 'nagovora', 'drugi', 'Želijo', 'pritisk', 'desetletjih', 'potrebne'].sort();
+
+function calculatePosition() {
+    var last = $('.word:first-child');
+    var lastposition = last.position();
+    lastposition['top'] = lastposition['top'] + 45;
+    lastposition['left'] = lastposition['left'] + last.width();
+
+    return lastposition;
+}
+
+$(document).ready(function() {
+
+    // $('.poetry').css('height', $(window).height());
+
+    // generate word blocks
+    $.each(words, function(i, word) {
+
+        // if (i === 0) {
+        $('.wordhouse').append('<div class="word">' + word + '</div>');
+        // } else {
+        //     var position = calculatePosition();
+        //
+        //     $('.poetry').append('<div class="word">' + word + '</div>').css({
+        //         'top': position['top'],
+        //         'left': position['left']
+        //     });
+        // }
+    });
+
+    // set doubleclick
+    $('.wordhouse').on('dblclick', '.word', function() {
+        // $('.wordhouse').prepend('<div class="word">' + $(this).text() + '</div>')
+
+        var position = calculatePosition();
+
+        $(this).clone().appendTo('.poetry');
+        $('.poetry .word').draggable();
+    });
+
+});
